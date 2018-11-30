0a5704d639f1e84123e373cc535196ed3327670d
lennux23 <alexochihua@hotmail.com>
2018-11-30 16:03:25 -0600

    add user service working

--- src/app.js
+++ src/app.js
@@ -11,16 +11,54 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
+// configuració db
+const mongo_conn = require('./mongoDBController')
+
+
+// conexión db
+var db = mongo_conn.connect();
+
+
+// schema usuario
+var User = require('../models/user')
+
+
 // servicio get user
 
 app.get('/user', (req,res) => {
   res.send (
     {
-      name: "Pablo",
+      name: "Victor",
       job: "Doctor"
     }
     
   )
+})
+
+// Crear usuario 
+app.post('/add_user', (req,res) => {
+  var db = req.db
+  var name = req.body.name
+  var job = req.body.job
+
+  var new_user = new User (
+    {
+      name: name,
+      job: job
+    }
+  )
+
+  new_user.save( (error) => {
+    if (error){
+      console.log(error)
+    } else {
+      console.log('Usuario insertado exitosamente')
+    }
+    res.send({
+      success: true,
+      message: "Usuario dado de alta exitosamente"
+    })
+  })
 })
 
 app.listen(8089)
